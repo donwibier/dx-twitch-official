ba74942b91a67e498658831311f14c2d3272d4e0
Don Wibier <don@anykey.nl>
2023-09-20 09:49:27 +0200

    Impmented the auto dark light theme toggling with DevExpress Blazor Project template

---
+++ DxBlazor.UI/wwwroot/dxblazor-ui.js
@@ -0,0 +1,9 @@
+﻿export function isDarkMode() {
+    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+}
+
+export function addThemeSwitchListener(dotNetReference) {
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+        dotNetReference.invokeMethodAsync("OsThemeSwitched", event.matches);
+    });
+}
