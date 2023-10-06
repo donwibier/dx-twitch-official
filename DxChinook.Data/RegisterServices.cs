82160181c4de240bcb5be8db7659e4c9b6fe385f
Don Wibier <don@anykey.nl>
2023-10-07 00:34:42 +0200

    Added Clientside validation with Blazored.FluentValidator

---
+++ DxChinook.Data/RegisterServices.cs
@@ -0,0 +1,21 @@
+﻿using DxChinook.Data.Models;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DxChinook.Data
+{
+    public static class RegisterServices
+    {
+        public static IServiceCollection RegisterModelValidators(this IServiceCollection services)
+        {            
+            services.AddScoped<IValidator<CustomerModel>, CustomerModelValidator>();            
+            
+            return services;
+        }
+    }
+}
