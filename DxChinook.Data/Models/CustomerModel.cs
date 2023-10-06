ca1e2ccc96b67adbe8563af6db76e3f185d84c4e
Don Wibier <don@anykey.nl>
2023-10-06 23:01:20 +0200

    Introduced IDataStore and EFStore to deal with DTO through AutoMapper and ServerSide Validation with FluentValidator

---
+++ DxChinook.Data/Models/CustomerModel.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DxChinook.Data.Models
+{
+    public class CustomerModel
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? Company { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Country { get; set; }
+        public string? PostalCode { get; set; }
+        public string? Phone { get; set; }
+        public string? Fax { get; set; }
+        public string Email { get; set; } = null!;
+
+        public string FullName { get => $"{LastName}, {FirstName}"; }
+    }
+    
+}
